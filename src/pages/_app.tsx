cd62f7fc57fb7c4c530210e1b4a6dd1f0e67487e
felixiho <okekefelix1@gmail.com>
2023-08-31 06:02:27 +0100

    remove fonts and home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
+
+import '@fontsource/outfit/300.css'
+import '@fontsource/outfit/400.css'
+import '@fontsource/outfit/500.css'
+import '@fontsource/outfit/600.css'
+import '@fontsource/outfit/700.css'
+import '@fontsource/outfit/800.css'
+import '@fontsource/outfit/900.css'
+
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import "@/styles/globals.css";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import type { AppProps } from "next/app"; 
 import theme from "@/config/chakra";
 import PageTransition from "@/components/layouts/PageTransition";
 import { CopyrightYearProvider } from "@/context/CopyrightYearContext";
@@ -26,18 +34,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <PageTransition>
       <ChakraProvider theme={theme}>
         <CopyrightYearProvider value={copyRightYear}>
-        <CSSReset />
-        <Head>
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-          <link
-            rel="preload"
-            href="/fonts/Larsseit.woff"
-            as="font"
-            type="font/woff"
-            crossOrigin=""
-          />
-          <link rel="preload" href="/fonts/Larsseit-light.woff2" as="font" />
-        </Head>
+        <CSSReset /> 
         <ErrorBoundary>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
         </CopyrightYearProvider>
       </ChakraProvider>
